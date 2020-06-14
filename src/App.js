dfc0973487486aca099741f2a623e9c4f3105495
Smiley <jcsmileyjr@gmail.com>
2020-06-14 11:21:45 -0500

    Added additional test data, restyle meetup container,

--- src/App.js
+++ src/App.js
@@ -20,7 +20,13 @@ function App() {
             <Meetup date={data.date} time={data.time} title={data.title} content={data.content} rsvp={data.rsvp} />
           )
         })
-        }        
+        }
+
+        <h3>
+          <a href="https://codeconnector.io/">Code Connector</a> is a non-profit that's organized tech meetups to help people 
+          start their journey into tech. You can join our daily conversations by clicking 
+          this link: <a href="https://bit.ly/2Ywnzqc">Code Connector slack channel</a>.
+        </h3>        
       </section>  
     </main>
   );
