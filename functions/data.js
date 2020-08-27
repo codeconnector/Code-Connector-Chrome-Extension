ebc24f8f4e229c9a59ea1e4254ce9952e5b0af72
Smiley <jcsmileyjr@gmail.com>
2020-08-27 05:13:45 -0500

    Update tech meetups for September 2020

--- functions/data.js
+++ functions/data.js
@@ -1,83 +1,58 @@
 const events = [
     {
-        date:"July 20, 2020",
+        date:"August 31, 2020",
         time:"12pm CST",
         title:"Happy Lunch Hour",
         content:"If you're interested in meeting with other developers and chatting about tech, life, etc. during your lunch break, you're more than welcome join us at our virtual table",
-        link:"https://us02web.zoom.us/j/82897733166",
-        rsvp:false
-    },
-    {
-        date:"July 27, 2020",
-        time:"12pm CST",
-        title:"Happy Lunch Hour",
-        content:"If you're interested in meeting with other developers and chatting about tech, life, etc. during your lunch break, you're more than welcome join us at our virtual table",
-        link:"https://us02web.zoom.us/j/82897733166",
-        rsvp:false
-    },
-    {
-        date:"July 28, 2020",
-        time:"6:30pm CST",
-        title:"Code Co-op",
-        content:"Sr. Agile Coach Steve Hallman will be giving an inspiring talk on using Trello for personal projects or in an Agile professional environment!",
-        link:"https://www.meetup.com/memphis-technology-user-groups/events/sdnpsrybckblc/",
+        link:"https://www.meetup.com/memphis-technology-user-groups/events/jtjlwrybclbpc/",
         rsvp:true
     },
-
     {
-        date:"August 1, 2020",
+        date:"September 5, 2020",
         time:"12pm CST",
         title:"Code Together | North MS",
         content:"This Code Together event is a time for you to work alongside and connect with other people in your local community. ",
-        link:" https://www.meetup.com/memphis-technology-user-groups/events/slpvsrybclbcb",
+        link:" https://www.meetup.com/memphis-technology-user-groups/events/xlvhwrybcmbhb/",
         rsvp:true
     },
     {
-        date:"Augest 3, 2020",
+        date:"September 7, 2020",
         time:"12pm CST",
         title:"Happy Lunch Hour",
         content:"If you're interested in meeting with other developers and chatting about tech, life, etc. during your lunch break, you're more than welcome join us at our virtual table",
-        link:"https://us02web.zoom.us/j/82897733166",
-        rsvp:false
+        link:"https://www.meetup.com/memphis-technology-user-groups/events/jtjlwrybcmbkb/",
+        rsvp:true
     },
     {
-        date:"August 9, 2020",
+        date:"September 13, 2020",
         time:"1:00pm CST",
         title:"Code Together with James Quick",
         content:"James Q. Quick is hosting a code-together where you can get help with code, or get advice on tech careers and startups.",
-        link:"https://www.meetup.com/memphis-technology-user-groups/events/nkpvsrybclbmb/",
+        link:"https://www.meetup.com/memphis-technology-user-groups/events/cmvhwrybcmbrb/",
         rsvp:true
     },
     {
-        date:"Augest 10, 2020",
+        date:"September 14 2020",
         time:"12pm CST",
         title:"Happy Lunch Hour",
         content:"If you're interested in meeting with other developers and chatting about tech, life, etc. during your lunch break, you're more than welcome join us at our virtual table",
-        link:"https://www.meetup.com/memphis-technology-user-groups/events/hdjtvrybclbnb/",
+        link:"https://www.meetup.com/memphis-technology-user-groups/events/jtjlwrybcmbsb/",
         rsvp:true
     },
     {
-        date:"Augest 17, 2020",
+        date:"September 21, 2020",
         time:"12pm CST",
         title:"Happy Lunch Hour",
         content:"If you're interested in meeting with other developers and chatting about tech, life, etc. during your lunch break, you're more than welcome join us at our virtual table",
-        link:"https://www.meetup.com/memphis-technology-user-groups/events/hdjtvrybclbwb/",
+        link:"https://www.meetup.com/memphis-technology-user-groups/events/jtjlwrybcmbcc/",
         rsvp:true
     },
     {
-        date:"Augest 24, 2020",
-        time:"12pm CST",
-        title:"Happy Lunch Hour",
-        content:"If you're interested in meeting with other developers and chatting about tech, life, etc. during your lunch break, you're more than welcome join us at our virtual table",
-        link:"https://www.meetup.com/memphis-technology-user-groups/events/hdjtvrybclbgc/",
-        rsvp:true
-    },
-    {
-        date:"August 25, 2020",
+        date:"September 22, 2020",
         time:"6:30pm CST",
         title:"Code Co-op",
         content:"This month we are presenting several lighting talks by multiple speakers such as Azhya Knox 'The Game Plan into Developer Land' and Matt Karmazyn 'Measuring the Success of DevOps in your Organization'.",
-        link:"https://www.meetup.com/memphis-technology-user-groups/events/sdnpsrybclbhc/",
+        link:"https://www.meetup.com/memphis-technology-user-groups/events/wbgfwrybcmbdc/",
         rsvp:true
     },
 ];
