940d2c9d567be0c1a7cf7d99c7076214e1770e7e
Smiley <jcsmileyjr@gmail.com>
2020-09-02 21:21:41 -0500

    Feature: show user date/time and time zone based on their location

--- functions/data.js
+++ functions/data.js
@@ -1,55 +1,48 @@
 const events = [
     {
-        date:"August 31, 2020",
-        time:"12pm CST",
+        date:[2020,8,31,12, 0],
         title:"Happy Lunch Hour",
         content:"If you're interested in meeting with other developers and chatting about tech, life, etc. during your lunch break, you're more than welcome join us at our virtual table",
         link:"https://www.meetup.com/memphis-technology-user-groups/events/jtjlwrybclbpc/",
         rsvp:true
     },
     {
-        date:"September 5, 2020",
-        time:"12pm CST",
+        date:[2020,9,5,13, 0],
         title:"Code Together | North MS",
         content:"This Code Together event is a time for you to work alongside and connect with other people in your local community. ",
         link:" https://www.meetup.com/memphis-technology-user-groups/events/xlvhwrybcmbhb/",
         rsvp:true
     },
     {
-        date:"September 7, 2020",
-        time:"12pm CST",
+        date:[2020,9,7,12, 0],
         title:"Happy Lunch Hour",
         content:"If you're interested in meeting with other developers and chatting about tech, life, etc. during your lunch break, you're more than welcome join us at our virtual table",
         link:"https://www.meetup.com/memphis-technology-user-groups/events/jtjlwrybcmbkb/",
         rsvp:true
     },
     {
-        date:"September 13, 2020",
-        time:"1:00pm CST",
+        date:[2020,9,13,13, 0],
         title:"Code Together with James Quick",
         content:"James Q. Quick is hosting a code-together where you can get help with code, or get advice on tech careers and startups.",
         link:"https://www.meetup.com/memphis-technology-user-groups/events/cmvhwrybcmbrb/",
         rsvp:true
     },
     {
-        date:"September 14 2020",
-        time:"12pm CST",
+        date:[2020,9,14,12, 0],
         title:"Happy Lunch Hour",
         content:"If you're interested in meeting with other developers and chatting about tech, life, etc. during your lunch break, you're more than welcome join us at our virtual table",
         link:"https://www.meetup.com/memphis-technology-user-groups/events/jtjlwrybcmbsb/",
         rsvp:true
     },
     {
-        date:"September 21, 2020",
-        time:"12pm CST",
+        date:[2020,9,7,21, 0],
         title:"Happy Lunch Hour",
         content:"If you're interested in meeting with other developers and chatting about tech, life, etc. during your lunch break, you're more than welcome join us at our virtual table",
         link:"https://www.meetup.com/memphis-technology-user-groups/events/jtjlwrybcmbcc/",
         rsvp:true
     },
     {
-        date:"September 22, 2020",
-        time:"6:30pm CST",
+        date:[2020,9,22,18, 30],
         title:"Code Co-op",
         content:"This month we are presenting several lighting talks by multiple speakers such as Azhya Knox 'The Game Plan into Developer Land' and Matt Karmazyn 'Measuring the Success of DevOps in your Organization'.",
         link:"https://www.meetup.com/memphis-technology-user-groups/events/wbgfwrybcmbdc/",
--- src/components/meetup/meetup.js
+++ src/components/meetup/meetup.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./meetup.css";
+const { DateTime } = require("luxon");
 
 const Meetup = (props) => {
+    const newDate = () => {
+        var dt = DateTime.local(props.date[0], props.date[1], props.date[2], props.date[3], props.date[4]);
+        return `${dt.toLocaleString(DateTime.DATETIME_FULL)}  ${dt.zoneName}`
+    }
+    
     return(
         <div className={props.current?'container currentMeetup':'container'}>
-            <p className="date-style">{props.date} at {props.time}</p>
+            <p className="date-style">{newDate()}</p>
             <h1>{props.title}</h1>
             <p className="content-style">{props.content}</p>
             {props.rsvp &&
